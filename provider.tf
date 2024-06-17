b89f85ab3c138babb9f0f2f36b403c958bc24891
Modest <maximodest@gmail.com>
2024-06-17 10:20:15 +0300

    Add TF Token

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
  provider "google" {
-  credentials = file("state.json")
+  credentials = file("./state.json")
   project     = "modest-dev-418814"
 }
  provider "google" {
-  credentials = file("cluster.json")
+  credentials = file("./cluster.json")
   project     = "tensile-will-419716"
   alias       = "gcp-service-project"
 }
