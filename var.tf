c6b46d7725646e1cf7d130a560e26cbe5015fc66
Modest <maximodest@gmail.com>
2024-06-17 12:16:25 +0300

    Add TF Token

--- var.tf
+++ var.tf
@@ -1,10 +1,13 @@
 variable "region" {
   description = "Deployment region"
+  type = string
   default = "us-central1-a"
 }
 variable "project" {
   description = "Project name"
+  type = string
 }
 variable "clusterName" {
   description = "Name of our Cluster"
+  type = string
 }
