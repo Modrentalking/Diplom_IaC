be7d9a2d6fc0416f0cd066491ec3599c63b0a9d2
Modest <maximodest@gmail.com>
2024-06-17 21:13:00 +0300

    Add node pool for 1ogging

--- main.tf
+++ main.tf
@@ -51,6 +51,35 @@ resource "google_container_cluster" "gke_cluster" {
     }
 
   }
+  node_pool {
+    initial_node_count = 1
+    node_count         = 1
+    name = "logging"
+
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
+    node_config {
+      disk_size_gb = 50
+      enable_confidential_storage = false
+      machine_type = "n1-standard-1"
+      service_account = "diplom@tensile-will-419716.iam.gserviceaccount.com"
+      spot = true 
+       
+      shielded_instance_config {
+        enable_integrity_monitoring = true
+        enable_secure_boot          = false
+      }
+      advanced_machine_features {
+        enable_nested_virtualization = false
+        threads_per_core             = 0
+      }
+    }
+    queued_provisioning { 
+        enabled = false 
+    }
+  }
   
 
   release_channel {
